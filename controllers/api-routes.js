fc58ff0835203140d1342f19e12a87b88d39539a
Sierra Chapman <siechap@gmail.com>
2020-08-05 19:15:12 -0700

    create API route to get featured projects

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -1,8 +1,14 @@
 // import dependencies 
 const express = require("express");
+const project = require("../models/project.js");
 
 // create router
 const router = express.Router();
+
+// create routes
+router.get("/api/projects/featured", (req, res) => {
+  res.send(project.getFeatured());
+});
 
 // export router
 module.exports = router;
