f5a70bcc39b80540a80991c2e1684cc8999d9044
Sierra Chapman <siechap@gmail.com>
2020-08-05 17:04:22 -0700

    serve simple handlebars pages

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -6,7 +6,11 @@ const router = express.Router();
 
 // add routes
 router.get("/", (req, res) => {
-  res.send("Home page!");
+  res.render("about", {});
+})
+
+router.get("/projects", (req, res) => {
+  res.render("projects", {});
 })
 
 // export router
